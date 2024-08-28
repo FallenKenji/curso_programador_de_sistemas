b8aefd13730a00b86a5ee8caa2e7f5f9ac664a80
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-08-28 16:39:08 -0300

    Finalização dos cenários do Exercício 10 da Lista 5

--- Exercícios/JavaScript/lib/Lista_5/TodosExercicios.js
+++ Exercícios/JavaScript/lib/Lista_5/TodosExercicios.js
@@ -1,15 +1,20 @@
 function separarPalavras(text, separador) {
 	return text.split(separador);
 }
-console.log(separarPalavras("O rato roeu a roupa do rei de Roma", " "));
-console.log(separarPalavras("Luís,Fernanda,Marcos", ","));
-console.log(separarPalavras("user1@gmail.com;user2@yahoo.com;user3@outlook.com", ";"));
-console.log(separarPalavras("Nome -- Sobrenome -- Idade -- País", " -- "));
-console.log(separarPalavras("/home/user/documents", "/"));
-console.log(separarPalavras("Eu gosto de programar", ""));
-console.log(separarPalavras("Programar é divertido. Pratique todos os dias.", ". "));
-console.log(separarPalavras("15/07/2005", "/"));
-console.log(separarPalavras("14:35:20", ":"));
-console.log(separarPalavras("Primeira linha\nSegunda linha\nTerceira linha", "\n"));
-console.log(separarPalavras("",));
-console.log(separarPalavras("",));
+
+console.log(superSplit("O rato roeu a roupa do rei de Roma", " "));   
+console.log(superSplit("João,Maria,José", ","));     
+console.log(superSplit("user1@gmail.com;user2@yahoo.com;user3@outlook.com", ";"));   
+console.log(superSplit("João,Maria,José", ","));   
+console.log(superSplit("Nome -- Sobrenome -- Idade -- País", " -- "));     
+console.log(superSplit("/usr/local/bin", "/"));    
+console.log(superSplit("JavaScript", ""));   
+console.log(superSplit("O sol está brilhando. O céu está azul. É um ótimo dia!", ". "));  
+console.log(superSplit("15/08/2024", "/"));  
+console.log(superSplit("14:35:20", ":"));    
+console.log(superSplit("Primeira linha\nSegunda linha\nTerceira linha", "\n"));   
+console.log(superSplit("Lorem ipsum dolor sit amet,\nconsectetur adipiscing elit,\nsed do eiusmod tempor incididunt ut labore.", "\n"));
+console.log(superSplit("Linha única sem quebras", "\n"));
+console.log(superSplit("Primeira linha de um texto.\n Segunda linha usando template strings.\n Terceira linha com mais detalhes.", "\n"));
+console.log(superSplit("Lorem ipsum dolor sit amet,\n consectetur adipiscing elit.\n Ut enim ad minim veniam.", "\n"));
+console.log(superSplit("Texto com linhas múltiplas e\n uso de templates strings\n para separação.", "\n"))
