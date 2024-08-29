9930843d2b0a3651655c3603e9eeb707c0774018
Kaiser <cleitinyeagerrumbling4@gmail.com>
2024-08-29 13:51:20 -0300

    Finalização do Exercicio 10 da Lista 4

--- Exercícios/JavaScript/lib/Lista_4/10_SomaPares.js
+++ Exercícios/JavaScript/lib/Lista_4/10_SomaPares.js
@@ -1,1 +1,16 @@
+function somaPares(num) {
+	let numero = num.filter(value => value % 2 == 0);
+
+	let total = 0;
+
+    for(let i=0; i<numero.length; i++) {
+        total += numero[i];
+    }
+
+    return total
+	}
+
+    console.log(somaPares([1, 2, 3, 4, 5]));
+    console.log(somaPares([2, 4, 6, 8]));
+    console.log(somaPares([1, 3, 5, 7]));
 
